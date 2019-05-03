982ce73318561fd4161475e75dbd6d02af88506a
yoo <javaking1129@nongshim.co.kr>
2019-05-03 15:21:18 +0900

    Initial commit

---
+++ src/main/java/com/web/boardservice/BoardserviceApplication.java
@@ -0,0 +1,14 @@
+package com.web.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class BoardserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardserviceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/web/boardservice/WebRestController.java
@@ -0,0 +1,14 @@
+package com.web.boardservice;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
