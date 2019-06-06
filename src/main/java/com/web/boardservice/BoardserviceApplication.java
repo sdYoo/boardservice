ccb12b5db659f7398af4806f3f23449bee736cd1
yoo-mac <megaboy1129@naver.com>
2019-06-06 22:06:15 +0900

    2019.06.06 sdyoo mac os -10:06

--- src/main/java/com/web/boardservice/BoardserviceApplication.java
+++ src/main/java/com/web/boardservice/BoardserviceApplication.java
@@ -3,7 +3,9 @@ package com.web.boardservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @Configuration
 @SpringBootApplication
 public class BoardserviceApplication {
--- src/main/java/com/web/boardservice/domain/posts/Posts.java
+++ src/main/java/com/web/boardservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.web.boardservice.domain.posts;
 
+import com.web.boardservice.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,7 @@ import javax.persistence.GeneratedValue;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
