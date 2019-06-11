e258e1f8b78468f22110e8481fcd3c7b1b2b9855
yoo <javaking1129@nongshim.co.kr>
2019-06-11 10:46:14 +0900

    2019.06.11 현재까지 개발된 소스 커밋 저장까지 완료

--- src/main/java/com/web/boardservice/service/PostsService.java
+++ src/main/java/com/web/boardservice/service/PostsService.java
@@ -1,11 +1,15 @@
 package com.web.boardservice.service;
 
+import com.dto.PostsMainResponseDto;
 import com.dto.PostsSaveRequestDto;
 import com.web.boardservice.domain.posts.PostsRepository;
 import lombok.AllArgsConstructor;
 import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -17,5 +21,12 @@ public class PostsService {
         System.out.println("Long save() init! ");
         System.out.println("title: "+dto.getTitle());
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/web/boardservice/webmain/WebController.java
+++ src/main/java/com/web/boardservice/webmain/WebController.java
@@ -1,16 +1,22 @@
 package com.web.boardservice.webmain;
 
+import com.web.boardservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
 
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
         System.out.println("web main controller!");
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "main";
     }
 }
