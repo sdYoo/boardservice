18397b3546effef41c9263521151ee7163b72ba7
yoo <javaking1129@nongshim.co.kr>
2019-06-11 09:33:41 +0900

    2019.06.11 현재까지 개발된 소스 커밋 저장까지 완료

--- src/main/java/com/web/boardservice/WebRestController.java
+++ src/main/java/com/web/boardservice/WebRestController.java
@@ -4,21 +4,23 @@ package com.web.boardservice;
 import com.dto.PostsSaveRequestDto;
 import com.web.boardservice.domain.posts.PostsRepository;
 import com.web.boardservice.service.PostsService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
     private PostsRepository postsRepository;
     private PostsService postsService;
 
-
-    public WebRestController(PostsRepository postsRepository) {
-        this.postsRepository = postsRepository;
-    }
+    //아래 생성자 주석 후 AllArgsConstructor 입력 시 정상적으로 저장됨
+    //public WebRestController(PostsRepository postsRepository) {
+    //    this.postsRepository = postsRepository;
+    //}
 
     @GetMapping("/hello")
     public String hello() {
@@ -30,6 +32,7 @@ public class WebRestController {
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
 
         //postsRepository.save(dto.toEntity());
+        System.out.println("savePosts!");
         return postsService.save(dto);
     }
 }
--- src/main/java/com/web/boardservice/service/PostsService.java
+++ src/main/java/com/web/boardservice/service/PostsService.java
@@ -3,6 +3,7 @@ package com.web.boardservice.service;
 import com.dto.PostsSaveRequestDto;
 import com.web.boardservice.domain.posts.PostsRepository;
 import lombok.AllArgsConstructor;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,8 @@ public class PostsService {
 
     @Transactional
     public Long save(PostsSaveRequestDto dto){
+        System.out.println("Long save() init! ");
+        System.out.println("title: "+dto.getTitle());
         return postsRepository.save(dto.toEntity()).getId();
     }
 }
